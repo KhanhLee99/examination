2339cb3bd18ea1a32834ce77c9a1099eb01bc0df
lvkhanh2 <lvkhanh2@cmcglobal.vn>
2024-03-19 11:58:56 +0700

    Updates

---
+++ server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"E6Kn2jginSyQto1QaHEv1lzC7dtW+SdTYxX41lTd3jI=\"}"
--- src/app/apiRequests/auth.ts
+++
@@ -1,8 +0,0 @@
-import http from "@/lib/http";
-import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
-
-const authApiRequest = {
-  login: (body: LoginBodyType) => http.post<LoginResType>("/auth/login", body),
-};
-
-export default authApiRequest;
